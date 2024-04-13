640e80869d310184a3948cea45041cfe07fd3ffd
MPulidoM <118181543+MPulidoM@users.noreply.github.com>
2024-04-13 00:09:31 -0500

    Update PublicationCRUD.java

--- src/main/java/org/acme/publication/PublicationCRUD.java
+++ src/main/java/org/acme/publication/PublicationCRUD.java
@@ -13,19 +13,41 @@ import static com.mongodb.client.model.Filters.eq;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase `PublicationCRUD` proporciona operaciones CRUD para documentos de publicaciones en una base de datos MongoDB.
+ */
 public class PublicationCRUD {
-
+    
+    /**
+     * Atributo `collectionPublication` es una colección de MongoDB para realizar operaciones CRUD en documentos de publicaciones.
+     */
     private final MongoCollection<Document> collectionPublication;
-
+    
+    /**
+     * Constructor para inicializar la clase `PublicationCRUD` con una instancia de `MongoDatabase`.
+     *
+     * @param database Instancia de `MongoDatabase`.
+     */
     public PublicationCRUD(MongoDatabase database) {
         this.collectionPublication = database.getCollection("posts");
     }
-
+    
+    /**
+     * Método para agregar una nueva publicación a la colección.
+     *
+     * @param at    El usuario que publica.
+     * @param message El mensaje de la publicación.
+     */
     public void addPublication(String at, String message) {
         Document nuevoPost = new Document("usuario", at).append("mensaje", message);
         collectionPublication.insertOne(nuevoPost);
     }
-
+    
+    /**
+     * Método para obtener una lista de todas las publicaciones en formato JSON.
+     *
+     * @return Representación de cadena de la lista de publicaciones en formato JSON.
+     */
     public String PublicationArray() {
         Gson json = new Gson();
         Bson projection = Projections.fields(Projections.include("usuario", "mensaje"), Projections.excludeId());
@@ -34,7 +56,13 @@ public class PublicationCRUD {
         String ans = json.toJson(posts);
         return ans;
     }
-
+    
+    /**
+     * Método para obtener las publicaciones de un usuario específico en formato JSON.
+     *
+     * @param at El usuario para el que se desean obtener las publicaciones.
+     * @return Representación de cadena de las publicaciones del usuario en formato JSON.
+     */
     public String getUserPublication(String at) {
         Bson projection = Projections.fields(Projections.include("usuario", "mensaje"),
                 Projections.excludeId());
@@ -44,7 +72,13 @@ public class PublicationCRUD {
         String ans = json.toJson(postsUsuario);
         return ans;
     }
-
+    
+    /**
+     * Método para eliminar una publicación específica de la colección.
+     *
+     * @param at    El usuario que publicó.
+     * @param message El mensaje de la publicación.
+     */
     public void DeletePost(String at, String message) {
         Bson filter = Filters.and(eq("usuario", at), eq("mensaje", message));
         collectionPublication.deleteOne(filter);
